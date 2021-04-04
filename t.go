6caff39df683226707cf2c18e201d764cfdb1644
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-04-03 23:38:17 -0400

    added mac dist

---
+++ t.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+    "flag"
+    "fmt"
+    "os"
+    "runtime"
+    "time"
+)
+
+var n = flag.Int("n", 1e5, "Number of goroutines to create")
+
+var ch = make(chan byte)
+var counter = 0
+
+func f() {
+    counter++
+    <-ch // Block this goroutine
+}
+
+func main() {
+    flag.Parse()
+    if *n <= 0 {
+            fmt.Fprintf(os.Stderr, "invalid number of goroutines")
+            os.Exit(1)
+    }
+
+    // Limit the number of spare OS threads to just 1
+    runtime.GOMAXPROCS(1)
+
+    // Make a copy of MemStats
+    var m0 runtime.MemStats
+    runtime.ReadMemStats(&m0)
+
+    t0 := time.Now().UnixNano()
+    for i := 0; i < *n; i++ {
+            go f()
+    }
+    runtime.Gosched()
+    t1 := time.Now().UnixNano()
+    runtime.GC()
+
+    // Make a copy of MemStats
+    var m1 runtime.MemStats
+    runtime.ReadMemStats(&m1)
+
+    if counter != *n {
+            fmt.Fprintf(os.Stderr, "failed to begin execution of all goroutines")
+            os.Exit(1)
+    }
+
+    fmt.Printf("Number of goroutines: %d\n", *n)
+    fmt.Printf("Per goroutine:\n")
+    fmt.Printf("  Memory: %.2f bytes\n", float64(m1.Sys-m0.Sys)/float64(*n))
+    fmt.Printf("  Time:   %f µs\n", float64(t1-t0)/float64(*n)/1e3)
+}
