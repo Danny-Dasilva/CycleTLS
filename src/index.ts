fd54351c85392b6cf0dab4b237ea166a072f1ec6
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-06-06 13:40:30 -0400

    lint

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import { spawn, exec, ChildProcessWithoutNullStreams } from 'child_process';
-import path from 'path';
-import { EventEmitter } from 'events';
-import { Server } from 'ws';
+import { spawn, exec, ChildProcessWithoutNullStreams } from "child_process";
+import path from "path";
+import { EventEmitter } from "events";
+import { Server } from "ws";
 export interface CycleTLSRequestOptions {
   headers?: {
     [key: string]: any;
@@ -29,7 +29,7 @@ const cleanExit = async (message?: string | Error) => {
   if (message) {
     console.log(message);
   }
- 
+
   if (process.platform == "win32") {
     new Promise((resolve, reject) => {
       exec(
@@ -46,15 +46,13 @@ const cleanExit = async (message?: string | Error) => {
   } else {
     //linux/darwin os
     new Promise((resolve, reject) => {
-      process.kill(-child.pid)
-      process.exit()
-
+      process.kill(-child.pid);
+      process.exit();
     });
   }
-      
 };
-process.on('SIGINT', () => cleanExit());
-process.on('SIGTERM', () => cleanExit());
+process.on("SIGINT", () => cleanExit());
+process.on("SIGTERM", () => cleanExit());
 
 class Golang extends EventEmitter {
   server: Server;
@@ -65,14 +63,14 @@ class Golang extends EventEmitter {
 
     let executableFilename;
 
-    if (process.platform == 'win32') {
-      executableFilename = 'index.exe';
-    } else if (process.platform == 'linux') {
-      executableFilename = 'index';
-    } else if (process.platform == 'darwin') {
-      executableFilename = 'index-mac';
+    if (process.platform == "win32") {
+      executableFilename = "index.exe";
+    } else if (process.platform == "linux") {
+      executableFilename = "index";
+    } else if (process.platform == "darwin") {
+      executableFilename = "index-mac";
     } else {
-      cleanExit(new Error('Operating system not supported'));
+      cleanExit(new Error("Operating system not supported"));
     }
 
     child = spawn(path.join(`"${__dirname}"`, executableFilename), {
@@ -82,25 +80,23 @@ class Golang extends EventEmitter {
       detached: true,
     });
 
-    child.stderr.on('data', (stderr) => {
-      if (stderr.toString().includes('Request_Id_On_The_Left')) {
-        const splitRequestIdAndError = stderr.toString().split('Request_Id_On_The_Left');
+    child.stderr.on("data", (stderr) => {
+      if (stderr.toString().includes("Request_Id_On_The_Left")) {
+        const splitRequestIdAndError = stderr.toString().split("Request_Id_On_The_Left");
         const [requestId, error] = splitRequestIdAndError;
         this.emit(requestId, { error: new Error(error) });
       } else {
         debug
           ? cleanExit(new Error(stderr))
           //TODO add Correct error logging url request/ response/ 
-          : cleanExit(new Error('Error Exiting ... (Golang wrapper exception)'));
+          : cleanExit(new Error("Error Exiting ... (Golang wrapper exception)"));
       }
     });
 
-    this.server.on('connection', (ws) => {
-      this.emit('ready');
-      
+    this.server.on("connection", (ws) => {
+      this.emit("ready");
 
-      ws.on('message', (data: string) => {
-
+      ws.on("message", (data: string) => {
         const message = JSON.parse(data);
         this.emit(message.RequestID, message.Response);
       });
@@ -117,7 +113,7 @@ class Golang extends EventEmitter {
   }
 
   exit() {
-    this.server.close()
+    this.server.close();
   }
 }
 
@@ -130,7 +126,7 @@ const initCycleTLS = async (
   (
     url: string,
     options: CycleTLSRequestOptions,
-    method?: 'head' | 'get' | 'post' | 'put' | 'delete' | 'trace' | 'options' | 'connect' | 'patch'
+    method?: "head" | "get" | "post" | "put" | "delete" | "trace" | "options" | "connect" | "patch"
   ): Promise<CycleTLSResponse>;
   head(url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse>;
   get(url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse>;
@@ -150,30 +146,29 @@ const initCycleTLS = async (
     if (!debug) debug = true;
 
     const instance = new Golang(port, debug);
-    instance.on('ready', () => {
+    instance.on("ready", () => {
       const CycleTLS = (() => {
         const CycleTLS = async (
           url: string,
           options: CycleTLSRequestOptions,
           method:
-            | 'head'
-            | 'get'
-            | 'post'
-            | 'put'
-            | 'delete'
-            | 'trace'
-            | 'options'
-            | 'connect'
-            | 'patch' = 'get'
+            | "head"
+            | "get"
+            | "post"
+            | "put"
+            | "delete"
+            | "trace"
+            | "options"
+            | "connect"
+            | "patch" = "get"
         ): Promise<CycleTLSResponse> => {
           return new Promise((resolveRequest, rejectRequest) => {
             const requestId = `${url}${Math.floor(Date.now() * Math.random())}`;
 
             if (!options.ja3)
-              options.ja3 =
-                '771,255-49195-49199-49196-49200-49171-49172-156-157-47-53,0-10-11-13,23-24,0';
-            if (!options.body) options.body = '';
-            if (!options.proxy) options.proxy = '';
+              options.ja3 = "771,255-49195-49199-49196-49200-49171-49172-156-157-47-53,0-10-11-13,23-24,0";
+            if (!options.body) options.body = "";
+            if (!options.proxy) options.proxy = "";
 
             instance.request(requestId, {
               url,
@@ -186,7 +181,8 @@ const initCycleTLS = async (
 
               const { Status: status, Body: body, Headers: headers } = response;
 
-              if (headers['Set-Cookie']) headers['Set-Cookie'] = headers['Set-Cookie'].split('/,/');
+              if (headers["Set-Cookie"])
+                headers["Set-Cookie"] = headers["Set-Cookie"].split("/,/");
 
               resolveRequest({
                 status,
@@ -197,34 +193,33 @@ const initCycleTLS = async (
           });
         };
         CycleTLS.head = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'head');
+          return CycleTLS(url, options, "head");
         };
         CycleTLS.get = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'get');
+          return CycleTLS(url, options, "get");
         };
         CycleTLS.post = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'post');
+          return CycleTLS(url, options, "post");
         };
         CycleTLS.put = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'put');
+          return CycleTLS(url, options, "put");
         };
         CycleTLS.delete = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'delete');
+          return CycleTLS(url, options, "delete");
         };
         CycleTLS.trace = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'trace');
+          return CycleTLS(url, options, "trace");
         };
         CycleTLS.options = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'options');
+          return CycleTLS(url, options, "options");
         };
         CycleTLS.connect = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'options');
+          return CycleTLS(url, options, "options");
         };
         CycleTLS.patch = (url: string, options: CycleTLSRequestOptions): Promise<CycleTLSResponse> => {
-          return CycleTLS(url, options, 'patch');
+          return CycleTLS(url, options, "patch");
         };
         CycleTLS.exit = async (): Promise<undefined> => {
-
           if (process.platform == "win32") {
             return new Promise((resolve, reject) => {
               exec(
@@ -239,14 +234,11 @@ const initCycleTLS = async (
               );
             });
           } else {
-
-          return new Promise((resolve, reject) => {
-            process.kill(-child.pid)
-            instance.exit()
-
-          });
-        }
-          
+            return new Promise((resolve, reject) => {
+              process.kill(-child.pid);
+              instance.exit();
+            });
+          }
         };
 
         return CycleTLS;
