b6a4cf8a09b9f910c065a5ec0b8b30577a492bdf
Danny-Dasilva <yahchayildasilva@gmail.com>
2021-10-06 04:55:31 -0400

    fix deps

--- main.go
+++ main.go
@@ -18,8 +18,8 @@ func main() {
 	client := cycletls.Init()
 	response, err := client.Do("https://tls13.1d.pw", cycletls.Options{
 		Body:      "",
-		Ja3:       "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
-		UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:65.0) Gecko/20100101 Firefox/65.0",
+		Ja3:       "771,4865-4866-4867-49195-49199-49196-49200-52393-52392-49171-49172-156-157-47-53,0-23-65281-10-11-35-16-5-13-18-51-45-43-27-21,29-23-24,0",
+		UserAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36",
 	}, "GET")
 	if err != nil {
 		log.Print("Request Failed: " + err.Error())
