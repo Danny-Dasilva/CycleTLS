273a5a9090427fcc1b7b65061398a1c5bf14930b
Danny-Dasilva <yahchayildasilva@gmail.com>
2022-02-08 12:32:54 -0500

    update unit tests

--- cycletls/tests/unit/roundtripper_test.go
+++ cycletls/tests/unit/roundtripper_test.go
@@ -5,6 +5,10 @@ import (
 	"testing"
 )
 
+const(
+	UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.106 Safari/537.36"
+
+)
 func assertEqual(t *testing.T, a interface{}, b interface{}) {
 	if a != b {
 		t.Fatalf("%s != %s", a, b)
@@ -12,7 +16,7 @@ func assertEqual(t *testing.T, a interface{}, b interface{}) {
 }
 
 func TestValidSpec(t *testing.T) {
-	spec, err := cycletls.StringToSpec("771,52244-52243-52245-49195-49199-158-49162-49172-57-49161-49171-51-156-53-47-10-255,0-23-35-13-5-13172-18-16-30032-11-10,23-24,0")
+	spec, err := cycletls.StringToSpec("771,52244-52243-52245-49195-49199-158-49162-49172-57-49161-49171-51-156-53-47-10-255,0-23-35-13-5-13172-18-16-30032-11-10,23-24,0", UserAgent)
 	if err != nil {
 		t.Fatal("Error with valid spec")
 	}
@@ -20,7 +24,7 @@ func TestValidSpec(t *testing.T) {
 }
 
 func TestInvalidSpec(t *testing.T) {
-	spec, err := cycletls.StringToSpec("771,52244-52243-52245-49195-49199-158-49162-49172-57-49161-49171-51-156-53-47-10-255,0-23-35-13-5-13172-18-16-111111-10,23-24,0")
+	spec, err := cycletls.StringToSpec("771,52244-52243-52245-49195-49199-158-49162-49172-57-49161-49171-51-156-53-47-10-255,0-23-35-13-5-13172-18-16-111111-10,23-24,0", UserAgent)
 	if err != nil {
 		assertEqual(t, err.Error(), "Extension {{ 111111 }} is not Supported by CycleTLS please raise an issue")
 	}
