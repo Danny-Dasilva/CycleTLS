d10c248461e384af8a551dc58bdd0d2c746b8a03
Danny-Dasilva <yahchayildasilva@gmail.com>
2022-04-20 00:33:54 -0400

    init

--- cycletls/tests/integration/custom_timeout_test.go
+++ cycletls/tests/integration/custom_timeout_test.go
@@ -19,7 +19,7 @@ func TestTimeoutSuccess(t *testing.T) {
 		Body:      "",
 		Ja3:       "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
 		UserAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
-		Timeout:   2,
+		Timeout:   5,
 	}, "GET")
 	if err != nil {
 		log.Print("Request Failed: " + err.Error())
@@ -32,7 +32,7 @@ func TestTimeoutSuccess(t *testing.T) {
 func TestTimeoutError(t *testing.T) {
 
 	client := cycletls.Init()
-	resp, err := client.Do("http://httpbin.org/delay/2", cycletls.Options{
+	resp, err := client.Do("http://httpbin.org/delay/4", cycletls.Options{
 		Body:      "",
 		Ja3:       "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
 		UserAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
--- cycletls/tests/integration/header_order_test.go
+++ cycletls/tests/integration/header_order_test.go
@@ -5,17 +5,17 @@ package cycletls_test
 
 import (
 	//"fmt"
+	"encoding/json"
 	"github.com/PuerkitoBio/goquery"
 	"log"
 	"strings"
 	"testing"
-	"encoding/json"
 
 	cycletls "github.com/Danny-Dasilva/CycleTLS/cycletls"
 )
 
 type HttpBinHeaders struct {
-    Headers   map[string]string      
+	Headers map[string]string
 }
 
 //TODO rewrite this so its not reliant on goquery
@@ -99,14 +99,13 @@ func TestCustomHeaderOrderFailure(t *testing.T) {
 	}
 }
 
-
 func TestCustomHeadersDefaultOrder(t *testing.T) {
 	client := cycletls.Init()
 	resp, err := client.Do("https://pgl.yoyo.org/http/browser-headers.php", cycletls.Options{
-		Body:        "",
-		Ja3:         "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
-		UserAgent:   "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
-		Headers:     map[string]string{"test1": "value1", "test2": "value2"},
+		Body:      "",
+		Ja3:       "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
+		UserAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
+		Headers:   map[string]string{"test1": "value1", "test2": "value2"},
 	}, "GET")
 	if err != nil {
 		log.Print("Request Failed: " + err.Error())
@@ -153,10 +152,10 @@ func TestCustomHeadersCustomOrder(t *testing.T) {
 func TestCustomHeaders(t *testing.T) {
 	client := cycletls.Init()
 	resp, err := client.Do("https://httpbin.org/headers", cycletls.Options{
-		Body:        "",
-		Ja3:         "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
-		UserAgent:   "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
-		Headers:     map[string]string{"foo": "bar"},
+		Body:      "",
+		Ja3:       "771,4865-4867-4866-49195-49199-52393-52392-49196-49200-49162-49161-49171-49172-51-57-47-53-10,0-23-65281-10-11-35-16-5-51-43-13-45-28-21,29-23-24-25-256-257,0",
+		UserAgent: "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:87.0) Gecko/20100101 Firefox/87.0",
+		Headers:   map[string]string{"foo": "bar"},
 	}, "GET")
 	if err != nil {
 		log.Print("Request Failed: " + err.Error())
