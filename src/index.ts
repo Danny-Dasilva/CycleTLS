31638dc8dad63502740e540b2efce48d8146cf19
Mario <me@marioparaschiv.com>
2024-07-05 22:53:47 +0100

    Fix process termination

--- src/index.ts
+++ src/index.ts
@@ -80,7 +80,7 @@ const handleSpawn = (debug: boolean, fileName: string, port: number, filePath?: 
   const execPath = filePath ?? path.join(__dirname, fileName);
   child = spawn(execPath, {
     env: { WS_PORT: port.toString() },
-    shell: true,
+    shell: false,
     windowsHide: true,
     detached: process.platform !== "win32"
   });
